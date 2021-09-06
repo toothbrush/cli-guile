239f1eee5c824d86d38a41a5cbdb55dec0d5536b
paul <423357+toothbrush@users.noreply.github.com>
2021-09-06 19:07:34 +1000

    Initial Commit

---
+++ cli-guile/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  cli-guile
+//
+//  Created by Paul on 6/9/21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
