ede81a5696e99d1475e9419cd351490c63b66499
paul <423357+toothbrush@users.noreply.github.com>
2021-09-06 19:24:01 +1000

    Wow, it works!

--- cli-guile/main.m
+++ cli-guile/main.m
@@ -9,18 +9,26 @@
 
 #import <libguile.h>
 
-void run_guile() {
+static void* register_functions (void* data)
+{
+    SCM test = scm_c_eval_string("(+ 3 5)");
+    int foo = scm_to_int(test);
+    printf("int foo = %d\n", foo);
+
+    scm_c_eval_string("(begin (display 'HelloWorld) (newline))");
+    return NULL;
+}
+
+void run_guile(void) {
     printf("hello from C, before Guile\n");
     scm_init_guile();
-    //scm_with_guile(&register_functions, NULL);
-    //scm_shell(0, NULL);
+    scm_with_guile(&register_functions, NULL);
 }
 
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         NSLog(@"Hello, World!");
-
         run_guile();
     }
     return 0;
