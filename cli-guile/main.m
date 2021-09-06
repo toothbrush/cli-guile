981036c033fc50e68f7b97e918f77281e05a3e64
paul <423357+toothbrush@users.noreply.github.com>
2021-09-06 19:20:20 +1000

    Add dependencies to build against libguile

--- cli-guile/main.m
+++ cli-guile/main.m
@@ -7,10 +7,21 @@
 
 #import <Foundation/Foundation.h>
 
+#import <libguile.h>
+
+void run_guile() {
+    printf("hello from C, before Guile\n");
+    scm_init_guile();
+    //scm_with_guile(&register_functions, NULL);
+    //scm_shell(0, NULL);
+}
+
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
         NSLog(@"Hello, World!");
+
+        run_guile();
     }
     return 0;
 }
